db518e05f5c4d5d8fa8da814844443a5e3a86b0c
Ramesh Chandra Majhee <40921575+rameshmajhee@users.noreply.github.com>
2026-04-18 21:29:37 +0530

    ss

--- src/main/java/com/example/employee/EmployeeBackendApplication.java
+++ src/main/java/com/example/employee/EmployeeBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeBackendApplication.class, args);
+        System.out.println("Employee Backend Application started successfully.");
     }
 }
 
