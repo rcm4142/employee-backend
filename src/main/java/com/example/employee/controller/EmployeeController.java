5c7db3656424f413ac1e07f1e938484a30ea5d0c
Ramesh Chandra Majhee <40921575+rameshmajhee@users.noreply.github.com>
2026-04-29 22:50:08 +0530

    test

--- src/main/java/com/example/employee/controller/EmployeeController.java
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.example.employee.service.EmployeeService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
